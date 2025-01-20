72da579cbaaebdcb15edb569ad1e117b5e0f98e7
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-01-20 19:11:16 +0100

    implement secCateg component to display multiple categories with dynamic data

--- frontend/src/Categories/secCateg.tsx
+++ frontend/src/Categories/secCateg.tsx
@@ -1,14 +1,51 @@
-import backpack from '../assets/Catimages/backpack-fl.webp'
-import arcetryx from '../assets/logos/Arcteryx-Logo.webp'
+import backpack from "../assets/Catimages/backpack-fl.webp";
+import arcetryx from "../assets/logos/Arcteryx-Logo.webp";
+import vapor from "../assets/Catimages/vapor-helmet.webp";
+import blackdiamond from "../assets/logos/black-diamond.webp";
 function secCateg() {
+  interface Category {
+    name: string;
+    colors: number;
+    price: number;
+    discount: number;
+    image: string;
+    logo: string;
+  }
+  const categoriesData: Category[] = [
+    {
+      name: "Alpha Fl 40 Backpack",
+      colors: 2,
+      price: 290,
+      discount: 189.9,
+      image: backpack,
+      logo: arcetryx,
+    },
+    {
+      name: "Vapor Helmet",
+      colors: 2,
+      price: 149.95,
+      discount: 192.00,
+      image: vapor,
+      logo: blackdiamond,
+    },
+  ];
+
   return (
     <>
-      <div className='bg-gray-200 w-96 p-10 pl-2'>
-          <div className='flex justify-center'><img src={backpack} width={250} alt="backpack" /></div>
-          <img src={arcetryx} width={70} alt="logo" />
-          <h3 className='text-3xl'>Alpha Fl 40 Backpack</h3>
-          <p>2 colors</p>
-          <p>$290 <span>$189.90</span></p>
+      
+      <div className="flex justify-center m-10">
+          {categoriesData.map((category, index) => (
+            <div key={index} className="bg-gray-200 w-96 p-10 pl-2 mr-10">
+              <div className="flex justify-center">
+                <img src={category.image} width={250} alt="backpack" />
+              </div>
+              <img src={category.logo} width={70} alt="logo" />
+              <h3 className="text-3xl">{category.name}</h3>
+              <p>{category.colors} colors</p>
+              <p>${category.price}</p>
+              <p> ${category.discount}</p>
+            </div>
+          ))}
       </div>
     </>
   );
