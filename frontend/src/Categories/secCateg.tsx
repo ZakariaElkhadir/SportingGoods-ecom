a3198c50af27bbe53d6f1995500b900787362f9b
ZakariaElkhadir <zelkhadir5@gmail.com>
2025-01-20 19:33:52 +0100

    Done: secCateg component to include new jacket category and enhance layout

--- frontend/src/Categories/secCateg.tsx
+++ frontend/src/Categories/secCateg.tsx
@@ -1,6 +1,8 @@
 import backpack from "../assets/Catimages/backpack-fl.webp";
 import arcetryx from "../assets/logos/Arcteryx-Logo.webp";
 import vapor from "../assets/Catimages/vapor-helmet.webp";
+import Mammut from "../assets/logos/mammut.webp";
+import MamJacket from "../assets/Catimages/mammut-jacket.webp";
 import blackdiamond from "../assets/logos/black-diamond.webp";
 function secCateg() {
   interface Category {
@@ -28,25 +30,42 @@ function secCateg() {
       image: vapor,
       logo: blackdiamond,
     },
+    {
+      name: "taiss hs hooded jacket",
+      colors: 2,
+      price: 350,
+      discount: 299.99,
+      image: MamJacket,
+      logo: Mammut,
+    },
   ];
 
   return (
     <>
-      
-      <div className="flex justify-center m-10">
-          {categoriesData.map((category, index) => (
-            <div key={index} className="bg-gray-200 w-96 p-10 pl-2 mr-10">
-              <div className="flex justify-center">
-                <img src={category.image} width={250} alt="backpack" />
+    <section className="m-12">
+        <h2 className="text-3xl text-sky-800 mb-2">ON SALE</h2>
+        <div className="flex justify-center gap-8 ">
+        
+            {categoriesData.map((category, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-lg w-96 h-[33rem] p-8 hover:shadow-xl transition-shadow duration-300">
+              <div className="relative">
+                <img src={category.logo} className="absolute top-4 left-4 w-16 object-contain" alt="brand logo" />
+                <div className="flex justify-center mb-6">
+                  <img src={category.image} className="w-72 h-72 object-contain" alt={category.name} />
+                </div>
               </div>
-              <img src={category.logo} width={70} alt="logo" />
-              <h3 className="text-3xl">{category.name}</h3>
-              <p>{category.colors} colors</p>
-              <p>${category.price}</p>
-              <p> ${category.discount}</p>
+              <div className="space-y-4">
+                <h3 className="text-2xl font-semibold capitalize">{category.name}</h3>
+                <p className="text-gray-600">{category.colors} colors available</p>
+                <div className="flex items-center space-x-4">
+                  <span className="text-gray-500 line-through">${category.price}</span>
+                  <span className="text-lg font-bold text-green-600">${category.discount}</span>
+                </div>
+              </div>
             </div>
-          ))}
-      </div>
+            ))}
+        </div>
+    </section>
     </>
   );
 }
