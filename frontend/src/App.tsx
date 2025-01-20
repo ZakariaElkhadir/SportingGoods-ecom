9603c8c9a7ac9805f162179e00c9da013d96892d
ChatkiAyman <aymanchatki@gmail.com>
2025-01-20 13:31:06 +0100

    add Tailwind CSS and Autoprefixer configuration; update App component styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className='text-3xl font-bold underline'>vffvr</h1>
+    <div className="bg-blue-500 text-white p-6">
+      Tailwind CSS is working!
+    </div>
   );
 }
 export default App
